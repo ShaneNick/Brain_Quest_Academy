184219c3e360b9599c90ab3d07474f13220b5c35
ShaneNick <shanetomonia@gmail.com>
2024-01-25 21:54:08 -0500

    added course detail page"

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.course_list, name = 'course_list'),
     path('create/', views.create_course, name = 'create_course'),
+    path('course/<int:pk>/', views.course_detail, name = 'course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render
 from .models import MakeCourse
 from .forms import CourseForm
+
 #view for listing courses
 def course_list(request):
     courses = MakeCourse.objects.all()
@@ -16,3 +18,8 @@ def create_course(request):
     else:
         form = CourseForm()
     return render(request, 'courses/create_course.html', {'form':form})
+
+#view for accesing the course
+def course_detail(request, pk):
+    course = get_object_or_404(MakeCourse, pk=pk)
+    return render(request, 'courses/course_detail.html', {'course':course })
